3881cc62e95086fe059fd9ee5bf78fb0a9a3695b
Reina Yamamoto <reina.yamamoto@horizon-cg.com>
2026-03-08 16:57:01 +0900

    ページネーションの作成

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator  
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import ArticleForm
@@ -5,9 +6,12 @@ from .models import Article
 
 def home(request):
     posts = Article.objects.all().order_by('-created_at')
+    paginator = Paginator(posts, 10)  # 10件ごと
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
     return render(request, 'myapp/home.html', {
         'title': 'ホーム画面',
-        'posts' : posts
+        'page_obj' : page_obj,
     })
 
 def post_article(request, pk):
